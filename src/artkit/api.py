6cd96ffd95b23f1bb52497410a263a0bd90a9a3f
j-ittner <ittner.jan@bcg.com>
2024-06-22 22:29:04 +0200

    API: re-add cision and diffusion packages to artkit.api

--- src/artkit/api.py
+++ src/artkit/api.py
@@ -36,6 +36,7 @@ __globals_original = set(globals().keys())
 
 
 from .flow import *
+from .model.diffusion import *
 from .model.diffusion.openai import *
 from .model.llm import *
 from .model.llm.anthropic import *
@@ -44,6 +45,7 @@ from .model.llm.groq import *
 from .model.llm.huggingface import *
 from .model.llm.multi_turn import *
 from .model.llm.openai import *
+from .model.vision import *
 from .model.vision.openai import *
 from .util import *
 
