f9b1793d80bbfd232b168b8a4d5e174efcf10917
Sean Anggani <seanggani@gmail.com>
2024-06-26 15:46:13 -0500

    API: Expose Diffusion, Vision and Completion Model

--- src/artkit/api.py
+++ src/artkit/api.py
@@ -37,10 +37,11 @@ __globals_original = set(globals().keys())
 
 from .flow import *
 from .model.diffusion import *
+from .model.diffusion.base import DiffusionModel
 from .model.diffusion.openai import *
 from .model.llm import *
 from .model.llm.anthropic import *
-from .model.llm.base import ChatModel
+from .model.llm.base import ChatModel, CompletionModel
 from .model.llm.gemini import *
 from .model.llm.groq import *
 from .model.llm.huggingface import *
@@ -48,6 +49,7 @@ from .model.llm.multi_turn import *
 from .model.llm.openai import *
 from .model.vision import *
 from .model.vision.openai import *
+from .model.vision.base import VisionModel
 from .util import *
 
 
